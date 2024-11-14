6f74aa8b6f90ea702ee0c4403f43ca3be3341e7e
SantiagoMartinez0626 <santiago26lopez06@gmail.com>
2024-11-13 20:14:12 -0500

    Front implementation and system functionality.

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, url_for
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 df = pd.read_csv('archive/covid19_tweets.csv')
 
-df = df[['id', 'text', 'created_at', 'user']]
+# Imprimir las columnas disponibles
+print("Columnas disponibles:", df.columns.tolist())
+
+# Asegúrate de usar las columnas correctas que existen en tu CSV
+df = df[['text']]  # Por ahora solo usamos 'text' que sabemos existe
 
 tweets = df.to_dict(orient='records')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/tweets', methods=['GET'])
 def get_tweets():
-    return jsonify(tweets)
-
-@app.route('/tweets/<int:tweet_id>', methods=['GET'])
-def get_tweet(tweet_id):
-    tweet = next((tweet for tweet in tweets if tweet['id'] == tweet_id), None)
-    if tweet:
-        return jsonify(tweet)
-    else:
-        return jsonify({'error': 'Tweet no encontrado'}), 404
+    offset = int(request.args.get('offset', 0))
+    limit = int(request.args.get('limit', 10))
+    return jsonify(tweets[offset:offset + limit])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
